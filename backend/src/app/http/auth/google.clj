4588eeb815686065baaf4d0fdb3ac7b9cbfe127b
Andrey Antukh <niwi@niwi.nz>
2021-02-07 19:00:32 +0100

    WIP

--- backend/src/app/http/auth/google.clj
+++ backend/src/app/http/auth/google.clj
@@ -111,6 +111,7 @@
 
     (let [method-fn (get-in rpc [:methods :mutation :login-or-register])
           profile   (method-fn {:email (:email info)
+                                :backend "google"
                                 :fullname (:fullname info)})
           uagent    (get-in request [:headers "user-agent"])
           token     (tokens :generate {:iss :auth
